34701a346e3fa556495e1998556c29a832ac1e18
Andrey Shkabura <shkaburaandrew@gmail.com>
2024-01-20 04:08:42 +0300

    langSyntax task complete

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int x = 0;
+        while (number != 0) {
+            x += number%10;
+            number /= 10;
+        }
+        number = x;
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,7 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        number = number%10 * 100 + (number /10)%10 *  10 + (number /100);
     }
 
 }
