50c7399daf280315424f087a4bae0adaaa17bfb0
Andrey Shkabura <shkaburaandrew@gmail.com>
2024-01-20 04:13:44 +0300

    some corrections to pass tests

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -2,13 +2,13 @@ package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
 
-    public void calculateSum(int number) {
+    public int calculateSum(int number) {
         int x = 0;
         while (number != 0) {
             x += number%10;
             number /= 10;
         }
-        number = x;
+        return x;
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,8 +2,9 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
-    public void revert(int number) {
+    public int revert(int number) {
         number = number%10 * 100 + (number /10)%10 *  10 + (number /100);
+        return number;
     }
 
 }
